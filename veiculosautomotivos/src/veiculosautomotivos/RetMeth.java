fc00387001d9d248ae95b194bb7a404c4f56373a
Samira <tisamirasilva@gmail.com>
2021-02-28 00:16:52 -0300

    modificado o método range() para retornar a autonomia dos objetos

---
+++ veiculosautomotivos/src/veiculosautomotivos/RetMeth.java
@@ -0,0 +1,30 @@
+package veiculosautomotivos;
+
+public class RetMeth {
+
+	public static void main(String[] args) {
+
+		Vehicle minivan = new Vehicle();
+		Vehicle sportcar = new Vehicle();
+
+		minivan.passengers = 7;
+		minivan.fuelcap = 16;
+		minivan.mpg = 21;
+
+		sportcar.passengers = 2;
+		sportcar.fuelcap = 14;
+		sportcar.mpg = 12;
+
+		// obtém as autonomias e atribui o valor retornado a uma variável
+		int range1 = minivan.range();
+		int range2 = sportcar.range();
+
+		System.out.println("Minivan can carry " + minivan.passengers + " with range of " + range1 + " miles");
+
+		System.out.println("Sportcar can carry " + sportcar.passengers + " with range of " + range2 + " miles");
+
+	}
+
+}
+//Resultado no console: Minivan can carry 7 with a range of 336 miles
+//Resultado no console: Sportcar can carry 2 with a range of 168 miles
--- veiculosautomotivos/src/veiculosautomotivos/Vehicle.java
+++ veiculosautomotivos/src/veiculosautomotivos/Vehicle.java
@@ -1,15 +1,13 @@
 package veiculosautomotivos;
 
-public class Vehicle { // adiciona range a Vehicle
+public class Vehicle { // usa valor de retorno
 
 	int passengers; // número de passageiros;
 	int fuelcap; // capacidade de armazenamento de combustível em galões;
 	int mpg; // consumo de combustível em milhas por galão
 
 	// exibe a autonomia 
-	void range () {
-		System.out.println("Range is " + fuelcap * mpg);
+	int range () {
+		return mpg * fuelcap;
 	}
 }
-// Resultado no console: Minivan can carry 7. Range is 336
-// Resultado no console: Sportcar can carry 2. Range is 336
